7c00d6e7e3cb894cc3a43d264523d53dc3e32106
Bence Feher <ben.feher@gmail.com>
2015-04-22 11:51:36 +0900

    All buttons are now formatted BFPaperButtons.

--- FRDLivelyButtonDemo/FRDLivelyButtonDemo/TestViewController.m
+++ FRDLivelyButtonDemo/FRDLivelyButtonDemo/TestViewController.m
@@ -47,18 +47,52 @@
 	// Do any additional setup after loading the view.
     
     [self.burgerButton setStyle:kFRDLivelyButtonStyleHamburger animated:NO];
+    self.burgerButton.isRaised = NO;
+    self.burgerButton.cornerRadius = MIN(CGRectGetWidth(self.burgerButton.bounds), CGRectGetHeight(self.burgerButton.bounds)) / 2.f;
+    
     [self.plusCircleButton setStyle:kFRDLivelyButtonStyleCirclePlus animated:NO];
+    self.plusCircleButton.isRaised = NO;
+    self.plusCircleButton.cornerRadius = MIN(CGRectGetWidth(self.plusCircleButton.bounds), CGRectGetHeight(self.plusCircleButton.bounds)) / 2.f;
+    
     [self.plustButton setStyle:kFRDLivelyButtonStylePlus animated:NO];
+    self.plustButton.isRaised = NO;
+    self.plustButton.cornerRadius = MIN(CGRectGetWidth(self.plustButton.bounds), CGRectGetHeight(self.plustButton.bounds)) / 2.f;
+    
     [self.closeButton setStyle:kFRDLivelyButtonStyleClose animated:NO];
+    self.closeButton.isRaised = NO;
+    self.closeButton.cornerRadius = MIN(CGRectGetWidth(self.closeButton.bounds), CGRectGetHeight(self.closeButton.bounds)) / 2.f;
+    
     [self.closeCircleButton setStyle:kFRDLivelyButtonStyleCircleClose animated:NO];
+    self.closeCircleButton.isRaised = NO;
+    self.closeCircleButton.cornerRadius = MIN(CGRectGetWidth(self.closeCircleButton.bounds), CGRectGetHeight(self.closeCircleButton.bounds)) / 2.f;
+    
     [self.upCaretButton setStyle:kFRDLivelyButtonStyleCaretUp animated:NO];
+    self.upCaretButton.isRaised = NO;
+    self.upCaretButton.cornerRadius = MIN(CGRectGetWidth(self.upCaretButton.bounds), CGRectGetHeight(self.upCaretButton.bounds)) / 2.f;
+    
     [self.downCaretButton setStyle:kFRDLivelyButtonStyleCaretDown animated:NO];
+    self.downCaretButton.isRaised = NO;
+    self.downCaretButton.cornerRadius = MIN(CGRectGetWidth(self.downCaretButton.bounds), CGRectGetHeight(self.downCaretButton.bounds)) / 2.f;
+    
     [self.leftCaretButton setStyle:kFRDLivelyButtonStyleCaretLeft animated:NO];
+    self.leftCaretButton.isRaised = NO;
+    self.leftCaretButton.cornerRadius = MIN(CGRectGetWidth(self.leftCaretButton.bounds), CGRectGetHeight(self.leftCaretButton.bounds)) / 2.f;
+    
     [self.rightCaretButton setStyle:kFRDLivelyButtonStyleCaretRight animated:NO];
+    self.rightCaretButton.isRaised = NO;
+    self.rightCaretButton.cornerRadius = MIN(CGRectGetWidth(self.rightCaretButton.bounds), CGRectGetHeight(self.rightCaretButton.bounds)) / 2.f;
+    
     [self.leftArrowButton setStyle:kFRDLivelyButtonStyleArrowLeft animated:NO];
+    self.leftArrowButton.isRaised = NO;
+    self.leftArrowButton.cornerRadius = MIN(CGRectGetWidth(self.leftArrowButton.bounds), CGRectGetHeight(self.leftArrowButton.bounds)) / 2.f;
+    
     [self.rightArrowButton setStyle:kFRDLivelyButtonStyleArrowRight animated:NO];
+    self.rightArrowButton.isRaised = NO;
+    self.rightArrowButton.cornerRadius = MIN(CGRectGetWidth(self.rightArrowButton.bounds), CGRectGetHeight(self.rightArrowButton.bounds)) / 2.f;
     
     [self.bigButton setStyle:kFRDLivelyButtonStyleClose animated:YES];
+    self.bigButton.isRaised = NO;
+    self.bigButton.cornerRadius = MIN(CGRectGetWidth(self.bigButton.bounds), CGRectGetHeight(self.bigButton.bounds)) / 2.f;
     [self.bigButton setOptions:@{kFRDLivelyButtonLineWidth: @(4.0f)}];
     
     FRDLivelyButton *button = [[FRDLivelyButton alloc] initWithFrame:CGRectMake(0,0,36,28)];
@@ -67,6 +101,8 @@
                           kFRDLivelyButtonColor: [UIColor blueColor]
                           }];
     [button setStyle:kFRDLivelyButtonStyleHamburger animated:NO];
+    button.isRaised = NO;
+    button.cornerRadius = MIN(CGRectGetWidth(button.bounds), CGRectGetHeight(button.bounds)) / 2.f;
     [button addTarget:self action:@selector(buttonAction:) forControlEvents:UIControlEventTouchUpInside];
     UIBarButtonItem *buttonItem = [[UIBarButtonItem alloc] initWithCustomView:button];
     self.navigationItem.rightBarButtonItem = buttonItem;
